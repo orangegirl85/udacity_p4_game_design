bb54fe66dd70d7ac34914e667bcab5a62bdb2668
Nicoleta Ianosi <nicoisonai@yahoo.com>
2016-08-19 22:17:47 +0300

    Create user functionality

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -18,6 +18,74 @@ var ticTacToeApp = ticTacToeApp || {};
 ticTacToeApp.controllers = angular.module('ticTacToeControllers', ['ui.bootstrap']);
 
 
+/**
+ * @ngdoc controller
+ * @name ModalUserCtrl
+ *
+ * @description
+ * Controller for showing Create User Modal.
+ *
+ */
+ticTacToeApp.controllers.controller('ModalUserCtrl', function($scope, $modal, $log){
+
+    $scope.createUser = function() {
+        var modalInstance = $modal.open({
+          templateUrl: '/partials/create-user.html',
+          controller: 'CreateUserInstanceCtrl',
+          scope: $scope
+        });
+
+        modalInstance.result.then(function () {
+            $log.info('Saved: ');
+        }, function () {
+          $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+});
+
+/**
+ * @ngdoc controller
+ * @name CreateUserInstanceCtrl
+ *
+ * @description
+ * Controller for creating a User.
+ *
+ */
+ticTacToeApp.controllers.controller('CreateUserInstanceCtrl', function($scope, $modalInstance, $log){
+    $scope.user = $scope.user || {};
+
+    $scope.submit = function (form) {
+            if(form.$invalid) {
+                return;
+            }
+            /**
+             * Invokes the tic_tac_toe.create_user method.
+             */
+            gapi.client.tic_tac_toe.create_user($scope.user).
+                execute(function (resp) {
+                    $scope.$apply(function () {
+                        if (resp.error) {
+                            // The request has failed.
+                            var errorMessage = resp.error.message || '';
+                            $scope.messages = 'Failed to create a user : ' + errorMessage;
+                            $scope.alertStatus = 'warning';
+                            $log.error($scope.messages );
+                        } else {
+                            // The request has succeeded.
+                            $scope.messages = resp.result.message;
+                            $scope.alertStatus = 'success';
+                            $scope.user = {};
+                            $log.info($scope.messages + ' : ' + JSON.stringify(resp.result));
+                        }
+                    });
+                });
+        };
+
+   $scope.close = function() {
+     $modalInstance.close();
+   }
+
+});
 
 /**
  * @ngdoc controller
